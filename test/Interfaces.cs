02c89f3b0b03e7b0221a7ebbbc2a9af47553993b
giopalaga <girgi1201@gmail.com>
2021-03-11 12:20:54 -0800

    Restructured And added IDisposable

--- test/Interfaces.cs
+++ test/Interfaces.cs
@@ -4,54 +4,68 @@ using System.Collections.Generic;
 
 namespace test
 {
-    public class Interfaces
+    // var nums = new List<int>() {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    // var enumerableOfNumbers = (IEnumerable<int>) nums;
+    // using var z = nums.GetEnumerator();
+
+    // var powerOfRandom = new PowerOfRandom(nums);
+    // foreach (var x in powerOfRandom)
+    // {
+    // Console.WriteLine(x);   
+    // }
+    public class Dis : IDisposable
     {
-        private class PowerOfRandom : IEnumerable<int>
+        private void Cleanup()
         {
-            private readonly List<int> _k;
             
-                    public PowerOfRandom(List<int> nums)
-                    {
-                        _k = nums;
-                    }
-                    public IEnumerator<int> GetEnumerator()
-                    {
-                        return new PowOfRandomEnumerator(_k);
-                    }       
-                 IEnumerator IEnumerable.GetEnumerator()
-                    {
-                        return GetEnumerator();
-                    }       
-                }
-
-                private class PowOfRandomEnumerator : IEnumerator<int>
-                {
-                    private readonly List<int> _l;
-                    public PowOfRandomEnumerator(List<int> ints)
-                    {
-                        _l = ints;
-                    }
-                    private static readonly Random Random = new Random();
-                    private readonly int _cRandom = Random.Next(2, 5);
-                    public int Current => (int)Math.Pow(_l[_index], _cRandom);
-                    private int _index = -1;
-            
-                    public bool MoveNext()
-                    {
-                        _index++;
-                        return _index < _l.Count;
-                    }
-                    public void Reset()
-                    {
-                        _index = 0;
-                    }
-                    public void Dispose()
-                    {
-                    }
-                    object IEnumerator.Current => Current;
-                }
-        
+        }
+        public void Dispose()
+        {
+        }
     }
+    internal class PowerOfRandom : IEnumerable<int>
+    {
+        private List<int> K { get; set; }
 
- 
+        public PowerOfRandom(List<int> nums)
+        {
+            K = nums;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            return new PowOfRandomEnumerator(K);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+    internal class PowOfRandomEnumerator : IEnumerator<int>
+                    {
+                        private readonly List<int> _l;
+                        public PowOfRandomEnumerator(List<int> ints)
+                        {
+                            _l = ints;
+                        }
+                        private static readonly Random Random = new Random();
+                        private readonly int _cRandom = Random.Next(2, 5);
+                        public int Current => (int)Math.Pow(_l[_index], _cRandom);
+                        private int _index = -1;
+                
+                        public bool MoveNext()
+                        {
+                            _index++;
+                            return _index < _l.Count;
+                        }
+                        public void Reset()
+                        {
+                            _index = 0;
+                        }
+                        public void Dispose()
+                        {
+                        }
+                        object IEnumerator.Current => Current;
+                    }
 }
--- test/Main.cs
+++ test/Main.cs
@@ -8,17 +8,7 @@ namespace test
     {
         static void Main()
         {
-            ApiMain.FileClasss.FileReader("C:/Users/Gio/Documents/c#/test/test/dummy.txt");
-            Console.WriteLine(ApiMain.J());
-            // var enumerableOfNumbers = (IEnumerable<int>) nums;
-            // using var enumeratorOfNumbers1 = nums.GetEnumerator();
-            // var nums = new List<int>() {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-            // using var z = nums.GetEnumerator();
-            // var powerOfRandom = new PowerOfRandom(nums);
-            // foreach (var x in powerOfRandom)
-            // {
-            //     Console.WriteLine(x);   
-            // }
+          
         }
     }
 }
